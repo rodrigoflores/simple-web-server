54581193cca0fc4cf97a177704bc8e39146cc417
Rodrigo Flores <flores@hey.com>
2024-10-05 12:54:17 -0300

    :sparkles: Random failure

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 from flask import Flask, request, jsonify
-import logging
+import random
 import os
 from pythonjsonlogger import jsonlogger
 
@@ -12,6 +12,21 @@ hostname = os.getenv('HOSTNAME', 'unknown')
 def request_data():
   app.logger.info('Request received')
   response = jsonify({'meta': {'hostname': hostname},
+                      'request_headers': dict(request.headers)})
+  return response, 200
+
+@app.route('/random-failure/<percentage>')
+def random_failure(percentage):
+  random_number = random.uniform(0, 1)
+  normalized_percentage = float(percentage) / 100.0
+  app.logger.info(f'Random number: {random_number}, Failure threshold: {normalized_percentage}')
+
+  status_code = 500 if random_number < normalized_percentage else 200
+  response = jsonify({'meta': {'hostname': hostname,
+                               'random_number': random_number,
+                               'normalized_percentage': normalized_percentage,
+                               'status_code': status_code,
+                               'percentage': percentage},
                       'request_headers': dict(request.headers)})
   return response, 200
 
